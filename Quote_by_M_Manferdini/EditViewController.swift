d5a674939aff8f5b1a777a0d067f5d87e3033dd0
rudifa <rudi.farkas@gmail.com>
2018-06-19 20:41:02 +0200

    add EditViewController

---
+++ Quote_by_M_Manferdini/EditViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditViewController.swift
+//  Quote_by_M_Manferdini
+//
+//  Created by Rudolf Farkas on 19.06.18.
+//  Copyright © 2018 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
